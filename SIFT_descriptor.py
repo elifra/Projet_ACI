59f36530f4b5f47fea265a653add6f9b49af4494
elifra <mael.levot@gmail.com>
2021-04-03 19:43:32 +0200

    descripteur SIFT

--- SIFT_descriptor.py
+++ SIFT_descriptor.py
@@ -1,24 +1,54 @@
 import cv2
 import numpy as np
 import os
+import skimage.io
 
-#def calculDescriptorSift() :
-images = os.listdir("D:\Docs\Documents\Etudes\ESIR_2\ACI\Projet\dataset")
+def calculDescriptorSift() :
+    images = os.listdir("D:\Docs\Documents\Etudes\ESIR_2\ACI\Projet\dataset")
 
-img = cv2.imread("dataset\\"+images[700])
-cv2.imshow('Image', img)
-cv2.waitKey()
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imshow('Image en ndg', gray)
-cv2.waitKey()
+    descripteurs = []
+    nb_keyPoints = []
+    for i in range(len(images)) :   
+        #print(i)
+        img = skimage.io.imread(fname="dataset\\"+images[i])
+        #cv2.imshow('Image', img)
+        #cv2.waitKey()
 
-sift = cv2.xfeatures2d.SIFT_create()
-keypoints_sift, descriptors = sift.detectAndCompute(gray, None)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        #cv2.imshow('Image en ndg', gray)
+        #cv2.waitKey()
 
-print("Nombre de points d intérets = " + str(len(keypoints_sift)))
-print(len(descriptors[0]))
+        sift = cv2.xfeatures2d.SIFT_create()
+        keypoints_sift, descriptors = sift.detectAndCompute(gray, None)
 
-img = cv2.drawKeypoints(gray,keypoints_sift, None)
-cv2.imshow('Images des points d interets', img)
-cv2.waitKey()
+        #print(descriptors)
+        if len(keypoints_sift) == 0 : print(images[i])
+        else :
+            nb_keyPoints.append(len(keypoints_sift))
+            for j in range(len(descriptors)) :
+                descripteurs.append(descriptors[j])
+
+        #print("Nombre de points d intérets = " + str(len(keypoints_sift)))
+        #print(len(descriptors[0]))
+
+        #img = cv2.drawKeypoints(gray,keypoints_sift, None)
+        #cv2.imshow('Images des points d interets', img)
+        #cv2.waitKey()
+
+    descripteurs_finaux = []
+    ind_nb_keyPoints = 0
+    ind_descripteurs = 0
+    #On parcourt le tableau contenant le nombre de points d'intérêts par image
+    while ind_nb_keyPoints < len(nb_keyPoints) :
+        #Si l'image i a moins de 10 points d'intérêts, on ne la garde pas et on passe à la suivante
+        if nb_keyPoints[ind_nb_keyPoints] < 10 :
+            ind_descripteurs = ind_descripteurs + nb_keyPoints[ind_nb_keyPoints]
+            ind_nb_keyPoints = ind_nb_keyPoints + 1
+        #Sinon on garde seulement 10 points d'intérêts de l'image et on passe à la suivante
+        else :
+            for l in range(10) :
+                descripteurs_finaux.append(descripteurs[ind_descripteurs+l])
+            ind_descripteurs = ind_descripteurs + nb_keyPoints[ind_nb_keyPoints]
+            ind_nb_keyPoints = ind_nb_keyPoints + 1
+    return descripteurs_finaux
--- script_projet.py
+++ script_projet.py
@@ -11,6 +11,7 @@ from statistics import mean
 
 import pre_processing
 import histoRGB_descriptor as d_histo
+import SIFT_descriptor as d_sift
 
 from sklearn.linear_model import LogisticRegression
 
@@ -46,10 +47,79 @@ def createFileDescripteurHisto() :
     
     table_data_descripteurRGB.close()
 
+def createFileDescripteurSIFT() :
+    #Création du fichier descripteur_SIFT
+    table_data_descripteurSIFT = open("descripteur_SIFT.txt", "w")
+    
+    #On récupère les descripteurs SIFT calculés pour chaque image
+    descript = d_sift.calculDescriptorSift()
+
+    #1281 = 1280 attributs (10*128) + colonne du label 
+    for i in range(1281):
+        if i==1280 : table_data_descripteurSIFT.write("label" + "\n")
+        else : table_data_descripteurSIFT.write("x" + str(i) + "   ")
+
+    #1102 = nombre d'images sélectionnées
+    ind_descript = 0
+    for i in range(1102) :
+        #On identifie les 10 points clés de l'image
+        p0 = descript[ind_descript]
+        p1 = descript[ind_descript + 1]
+        p2 = descript[ind_descript + 2]
+        p3 = descript[ind_descript + 3]
+        p4 = descript[ind_descript + 4]
+        p5 = descript[ind_descript + 5]
+        p6 = descript[ind_descript + 6]
+        p7 = descript[ind_descript + 7]
+        p8 = descript[ind_descript + 8]
+        p9 = descript[ind_descript + 9]
+
+        #On remplie le fichier des 10 descripteurs par image (10*128 attributs par ligne)
+        for ind in range(len(p0)) :
+            table_data_descripteurSIFT.write(str(p0[ind]) + "   ")
+        
+        for ind in range(len(p1)) :
+            table_data_descripteurSIFT.write(str(p1[ind]) + "   ")
+        
+        for ind in range(len(p2)) :
+            table_data_descripteurSIFT.write(str(p2[ind]) + "   ")
+        
+        for ind in range(len(p3)) :
+            table_data_descripteurSIFT.write(str(p3[ind]) + "   ")
+        
+        for ind in range(len(p4)) :
+            table_data_descripteurSIFT.write(str(p4[ind]) + "   ")
+        
+        for ind in range(len(p5)) :
+            table_data_descripteurSIFT.write(str(p5[ind]) + "   ")
+        
+        for ind in range(len(p6)) :
+            table_data_descripteurSIFT.write(str(p6[ind]) + "   ")
+        
+        for ind in range(len(p7)) :
+            table_data_descripteurSIFT.write(str(p7[ind]) + "   ")
+        
+        for ind in range(len(p8)) :
+            table_data_descripteurSIFT.write(str(p8[ind]) + "   ")
+        
+        for ind in range(len(p9)) :
+            if ind==len(p9)-1 and i<1101: table_data_descripteurSIFT.write(str(p9[ind]) + "   " + str(pre_processing.labels[i]) + "\n")
+            elif ind==len(p9)-1 and i==1101 : table_data_descripteurSIFT.write(str(p9[ind]) + "   " + str(pre_processing.labels[i]))
+            else : table_data_descripteurSIFT.write(str(p9[ind]) + "   ")
+        ind_descript = ind_descript + 10
+
+    table_data_descripteurSIFT.close()
 
 if __name__ == "__main__":
+    """
     createFileDescripteurHisto()
     
     table_descripteurRGB = np.loadtxt("descripteur_histoRGB.txt", dtype="str")
     print(table_descripteurRGB[:1124])
     print("Taille de la table = " + str(table_descripteurRGB.shape))
+    """
+    createFileDescripteurSIFT()
+
+    table_descripteurSIFT = np.loadtxt("descripteur_SIFT.txt", dtype="str")
+    print(table_descripteurSIFT[:1103])
+    print("Taille de la table = " + str(table_descripteurSIFT.shape))
